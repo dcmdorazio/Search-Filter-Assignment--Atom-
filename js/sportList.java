e133ad284f6c5f92be899334a3bccac9b48be21b
Cody D'Orazio <cxdorazio@gmail.com>
2019-05-11 16:58:58 -0400

    cleaned up the CSS and HTML files.

--- js/sportList.java
+++ js/sportList.java
@@ -1,9 +1,10 @@
-/*Author: Cody D'Orazio
-    Assignment: Search filter project
-    Date last edited: 5/4/19
-    */
+// Author: Cody D'Orazio
+//     Assignment: Search filter project
+//     Date last edited: 5/4/19
+//
 
 // Sport Variables for HTML format
+//<!DOCTYPE java.util.>
 var sport = [
     'Lacrosse',
     'Soccer',
